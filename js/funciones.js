cb10674c64125be31c4aaa5e11d0cb33f25186ca
PlantaH <laplantah@gmail.com>
2021-09-24 17:20:18 -0300

    productos

---
+++ js/funciones.js
@@ -0,0 +1,18 @@
+/* */
+const textosBanners =  document.getElementById("textosBanners");
+
+const ocultarMensajeBanner = () => {
+   
+    if($('#textosBanners').css('display') == 'none'){
+        $("#textosBanners").show();
+     }else{
+        $("#textosBanners").hide();
+     }
+
+   
+}
+/* */
+
+const verMasCategoria = (m) =>{
+    window.location = 'productos.html'
+}
